577f991279d9dc6dad347a8812a18a2c962ce663
Patrik Riström <patrik@damn.moe>
2017-10-21 10:12:55 +0200

    Animated the bars, tweening them every 100ms

--- src/visualizer.ts
+++ src/visualizer.ts
@@ -1,18 +1,27 @@
+import { TweenLite, Sine } from "gsap";
+
 export default class Visualizer {
   canvas: HTMLCanvasElement;
   context: CanvasRenderingContext2D;
   fftSize: number;
+  framesSinceLastUpdate: number;
   analyser: any;
+  updateRate: number; // How often the bars will update, in milliseconds
+  audioData: Array<number>;
+  barHeights: Array<number>;
 
   constructor(canvasElement: HTMLCanvasElement) {
     this.canvas = canvasElement;    
+
+    this.updateRate = 100;
     
     // Makse sure canvas is always the correct size
     this.resizeCanvas();
     window.addEventListener("resize", this.resizeCanvas.bind(this));
 
     // The resolution of the waveform (the lower, the "blockier")
-    this.fftSize = 128;
+    this.fftSize = 256;
+    this.framesSinceLastUpdate = 0;
 
     this.context = canvasElement.getContext("2d");
 
@@ -26,7 +35,16 @@ export default class Visualizer {
         this.analyser.fftSize = this.fftSize;
       });
 
+    // Initiliaze audio data and bar heights
+    this.audioData = [];
+    this.barHeights = [];
+    for (var index = 0; index < this.fftSize; index++) {
+      this.barHeights.push(0);
+    }
+
+    // Set intervals
     window.requestAnimationFrame(this.draw.bind(this));
+    setInterval(this.update.bind(this), this.updateRate);
   }
 
   /**
@@ -37,19 +55,44 @@ export default class Visualizer {
     this.canvas.height = window.innerHeight;
   }
 
-  draw() {
+  /**
+   * Gets the latest audio data from the audio stream
+   */
+  update() {
     if (this.analyser) {
-      const barWidth = document.body.clientWidth / (this.fftSize / 2);
-      const margin = 2;
       const bufferLength = this.analyser.frequencyBinCount;
       const dataArray = new Uint8Array(bufferLength);
       this.analyser.getByteTimeDomainData(dataArray);
 
-      this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
-
+      this.audioData = [];
+      
       for (var i = 0; i < bufferLength; i++) {
-        var height = (dataArray[i] - 128) * 20;
-        this.context.fillStyle = `rgb(${dataArray[i]}, ${dataArray[i]}, ${dataArray[i]})`;
+        this.audioData.push(dataArray[i]);
+
+        const newHeight = Math.max(0, (dataArray[i] - 128) * 15);
+        let bar = { index: i, height: this.barHeights[i] };
+        
+        TweenLite.to(bar, this.updateRate / 1000, {
+          height: newHeight,
+          onUpdate: () => {
+            this.barHeights[bar.index] = bar.height;
+          },
+          ease: Sine.easeInOut
+        });
+      }
+    }
+  }
+
+  draw() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    const barWidth = this.canvas.width / (this.fftSize / 2);
+    const margin = 2;
+
+    if (this.barHeights) {
+      for (var i = 0; i < this.barHeights.length; i++) {
+        const height = this.barHeights[i];
+        this.context.fillStyle = `rgb(255, 255, 255)`;
         this.context.fillRect(i * (barWidth + margin), this.context.canvas.height - (height/2), barWidth, height);
       }
     }
